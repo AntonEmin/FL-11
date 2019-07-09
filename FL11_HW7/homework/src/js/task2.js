0d9f600e7ce6880284f4cbd28c26f0b475826f11
Anton Emin <anton.emin.work@gmail.com>
2019-07-09 21:28:12 +0300

    Fix some bugs

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -11,11 +11,11 @@ if (confirm('Do you want to play a game?')) {
     let totalPrize = 0;  
     let wantPlayGame = true;
     let gameWin = false;
-    do {
+    while(wantPlayGame === true) {
         let randomRange = startRange + rangeStep*loop;
         let randomNumber = Math.floor(Math.random() * randomRange);
         console.log(randomNumber);        
-        do {
+        while(attempts > 0) {
             let prizePull = 0;
             const prizePullStep = 2;
             if (attempts === firstAttempt) {
@@ -37,13 +37,16 @@ if (confirm('Do you want to play a game?')) {
             } else {
                 alert('You guessed wrong number.')
             }
-            attempts -= 1;
-        } while(attempts > 0)
+            attempts -= 1;                           
+            
+        } 
         if (gameWin) {
             if (confirm('Congratulation, you won!\nYour prize is: '+ totalPrize +'$\nDo you want to continue?')) {
                 loop += 1;
+                gameWin = false;
                 attempts = firstAttempt;
             } else {
+                totalPrize = 0;
                 alert('Thank you for your participation.\nYour prize is: '+ totalPrize +'$');
                 if (confirm('Do you want to play again?')) {
                     loop += 1;
@@ -53,14 +56,16 @@ if (confirm('Do you want to play a game?')) {
                 }
             }
         } else {
+            totalPrize = 0;
             alert('Thank you for your participation.\nYour prize is: '+ totalPrize +'$');
             if (confirm('Do you want to play again?')) {
-                attempts =firstAttempt;
+                attempts = firstAttempt;
+                loop = 0;
             } else {
                 wantPlayGame = false;
             }
         }
-    }while(wantPlayGame === true)
+    }
 } else {
     alert('You did not become a billionaire, but can.');
 }
