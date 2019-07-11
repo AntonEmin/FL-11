e4a13d02b59ecc05a592ed16820ad18daa74e929
Anton Emin <anton.emin.work@gmail.com>
2019-07-11 19:05:41 +0300

    HW8 done

--- FL11_HW8/homework/js/formatTime.js
+++ FL11_HW8/homework/js/formatTime.js
@@ -1,6 +1,6 @@
 function formatTime(a) {
     let minutes = a % 60;
-    let hours = ((a - minutes) / 60) %24;
+    let hours = ((a - minutes) / 60) % 24;
     let days = ((( a - minutes) / 60) - hours) / 24;
     return days + ' day(s) '+ hours + ' hour(s) ' + minutes +' minute(s)'
 }
